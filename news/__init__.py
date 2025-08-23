51b6bf727bc5428d74144f9712185da7afcb267c
elyx5534 <adilemresahin5555@gmail.com>
2025-08-23 04:43:10 +0300

    fix: add __init__.py to news module

---
+++ news/__init__.py
@@ -0,0 +1,4 @@
+# News module
+from .sentiment_analyzer import NewsAggregator, SentimentAnalyzer, NewsAlertSystem, NewsArticle, SentimentScore
+
+__all__ = ['NewsAggregator', 'SentimentAnalyzer', 'NewsAlertSystem', 'NewsArticle', 'SentimentScore']
